1473068a94de35542139a5759a7d3c51690bc065
Ethan Muchnik <emuchnik@andrew.cmu.edu>
2022-01-03 10:59:24 -0500

    Modified the directions

--- CodeRunner.py
+++ CodeRunner.py
@@ -51,23 +51,40 @@ class Runner:
 
         for row in range(self.size):
             for col in range(self.size):
-                self.modMat[5][row][col] = self.inMat[row+(self.size*3+1)+1][col+(self.size+1)+1]
+                self.modMat[5][row][col] = self.inMat[row+(self.size+1)*3+1][col+(self.size+1)+1]
     
     def startRunner(self):
+        self.runnerSetup()
+        self.printMod()
+        self.moveLoop()
+
+    def runnerSetup(self):
         self.initModMat()
         for row in range(self.size):
             for col in range(self.size):
-                if self.modMat[self.side.value][row][col] == "(":
+                if self.modMat[self.side.value][row][col] == '(':
                     self.pos.xCord = col
                     self.pos.yCord = row
+                    print("init col is" + str(col))
+                    print("init row is" + str(row))
                     return
+                print("did find")
+        print("havent found it")
 
 
-    def move(self):
-        self.nextPos()
+    def moveLoop(self):
+        toContinue = True
+        while toContinue:
+            self.nextPos()
+            toContinue = self.interpChar()
+            print(self.direction)
+            print(self.side)
+            print("toContinue is " + str(toContinue) + "\n")
+
+        print("Program has ended")
 
     def nextPos(self):
-        if self.direction == self.Directions.up:
+        if self.direction == self.Directions.down:
             if self.inBounds(self.pos.yCord+1):
                 self.pos.yCord+=1
             else:
@@ -82,8 +99,8 @@ class Runner:
                 self.pos.xCord+=1
             else:
                 self.switchSides()
-        elif self.direction == self.Directions.down:
-            if self.inBounds(int(self.pos.xCord-1)):
+        elif self.direction == self.Directions.up:
+            if self.inBounds(int(self.pos.yCord-1)):
                 self.pos.yCord-=1
             else:
                 self.switchSides()
@@ -95,7 +112,7 @@ class Runner:
             return True
 
 
-    def switchSides():
+    def switchSides(self):
         if self.side == self.Sides.bottom:
             if self.direction == self.Directions.up:
                 self.direction = self.Directions.up
@@ -208,11 +225,34 @@ class Runner:
                 self.side = self.Sides.top
                 self.pos.yCord = 0
 
-    def interpChar():
-        pass
+    def interpChar(self):
+        charAtPos = self.modMat[self.side.value][self.pos.yCord][self.pos.xCord]
+        print("char at position is " + charAtPos)
+        # print("char at position is " + str(self.pos.yCord))
+        # print("char at position is " + str(self.pos.xCord))
+        toCont = True
+        if charAtPos == ' ':
+            pass
+        elif(charAtPos == '>'):
+            self.direction = self.Directions.right
+            # print("dir changed to right")
+        elif(charAtPos == '<'):
+            self.direction = self.Directions.left
+            # print("dir changed to left")
+        elif(charAtPos == '^'):
+            self.direction = self.Directions.up
+            # print("dir changed to up")
+        elif(charAtPos == 'v'):
+            self.direction = self.Directions.down
+            # print("dir changed to down")
+        elif(charAtPos == ')'):
+            toCont = False
+
+        return toCont
     
     def printMod(self):
         print(self.modMat)
+        print("\n")
     
     def fancyPrint(self):
         pass
@@ -231,8 +271,8 @@ class Runner:
         right = 2
         down = 3
     
-    side = Sides.back
-    direction = Directions.up
+    side = Sides.left
+    direction = Directions.right
 
 def main():
     fileName = input("Give the Cube File Name ")
@@ -246,8 +286,6 @@ def main():
 
     runner = Runner(myMatrix,inter.size)
     runner.startRunner()
-    runner.move()
-    runner.printMod()
     # if len(sys.argv) ==2: readFile.read(sys.argv[1])
     # else: print("Usage:", sys.argv[0], "filename")
 
